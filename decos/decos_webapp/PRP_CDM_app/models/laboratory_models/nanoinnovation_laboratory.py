0215f56091137fb3583536be0e0046682210dcbf
Marco Prenassi <m.prenassi@gmail.com>
2025-07-31 13:31:46 +0200

    NIL lab with dynamic views added and changed textbox css

--- decos/decos_webapp/PRP_CDM_app/models/laboratory_models/nanoinnovation_laboratory.py
+++ decos/decos_webapp/PRP_CDM_app/models/laboratory_models/nanoinnovation_laboratory.py
@@ -76,9 +76,7 @@ class Nanoinnovation_LaboratorySamples(Samples):
 
     # Support
     support = models.CharField(max_length=256, blank=True)
-
-
-    submitted_at = models.DateTimeField(auto_now_add=True)
+    submitted_at = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
         return f"Sample ({self.get_sample_type_display()})"
